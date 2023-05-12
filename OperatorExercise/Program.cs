4c6d11a7cea45106b9b12141e87adbc7aa92f9e1
Mdriscoll95 <michael.driscoll951@gmail.com>
2023-05-12 13:00:41 -0700

    hw

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
+     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int division = a / b;
+            int mod = a % b;
+            Console.WriteLine($"{a}/{b} = {division} remainder {mod}");
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+           
+           
+            double myarea = AreaOfCircle(3);
+            Console.WriteLine(myarea);
+
+            Console.WriteLine(myarea + b);
+            Console.WriteLine(myarea * b);
+            Console.WriteLine(myarea - b);
+            Console.WriteLine(myarea / b);
+            Console.WriteLine(myarea % b);
+
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            Console.WriteLine(k);
         }
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+            
+        }
+        
+
+
     }
 }
